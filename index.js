6c2b2bbe72c253ed4c1857408bc48247c94e62a1
MWegenke <matthew.wegenke@galvanize.com>
2026-05-04 09:38:21 -0500

    small morning intro

---
+++ index.js
@@ -0,0 +1,27 @@
+// let name = "Matt"
+// let job = 'instructor'
+// console.log(name + "is an" + job)
+// console.log(`${name} is an ${2 + 6}`)
+
+function makeName (first, last){
+  fullName = `${first} ${last}`
+}
+
+let fName = "Connor"
+let lName = "Arretche"
+
+let fullName = "Natalya"
+
+console.log(fullName)
+
+makeName("Russ", "Oster")
+
+console.log(fullName)
+
+// let fullName = `${fName} ${lName}`
+
+// function sayHello (name) {
+//   console.log(name)
+// }
+// // console.log(fullName)
+// sayHello(56432106354321)
